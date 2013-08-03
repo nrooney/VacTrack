e8502f1a4c333259d90cc742ca98f3cb35a08e19
Awad Mackie <firesock.serwalek@gmail.com>
2013-08-03 01:51:01 +0100

    Finish first stab at schema

--- server/server.js
+++ server/server.js
@@ -8,18 +8,20 @@ port = 8000;
 var db_file = "./vactrack.db"
 
 if (!fs.existsSync(db_file)) {
-	console.log("No db! Create " + db_file);
-	// TODO: create a quicksetup for the sqlite file
+	console.log("No db! Create by executing \"sqlite3 -init server/vactrack.sql " + db_file + " '.exit'\"");
+	// TODO: create dummy data for insert
 	process.exit(1);
 }
 
 // afaict, C++ destructor for Database closes sqlite handles.
 // What are Node's guarantees on calling those...?
 // Attempting to call close() in SIGINT seems to restart the event loop
-var db = new sqlite3.Database(db_file, function(error) {
-	if (!error) {
-		start_server();
-	} // TODO: Meaningful error goes here
+var db = new sqlite3.Database(db_file, function(open_error) {
+	if (!open_error) {
+		db.exec("PRAGMA foreign_keys = ON;", function (init_error) {
+			if (!init_error) start_server();
+		});
+	} // TODO: Meaningful error messages
 });
 
 function start_server() {
--- server/vactrack.sql
+++ server/vactrack.sql
@@ -1,32 +1,50 @@
 
-CREATE TABLE tPerson
-(
-id int NOT NULL,
-name varchar(255) NOT NULL,
-
-PRIMARY KEY (id)
-)
 
 
+  -- +-------------------------+                  +----------------------+
+  -- | Person                  |                  | Event                |
+  -- |-------------------------|                  |----------------------|
+  -- | name                    |----------------->| name                 |
+  -- |                         |         ^        | coords               |
+  -- +-------------------------+         |        | range                |
+  --                                     |        | (extra_details)      |
+  --                                     |        | time_stamp           |
+  --                                     +        +----------------------+
+  --                             +--------------+
+  --                             | Transaction  |
+  --                             |--------------|
+  --                             | amount       |
+  --                             |              |
+  --                             +--------------+
 
-CREATE TABLE tEvent
-(
-id int NOT NULL,
-name varchar(255) NOT NULL,
-time_stamp DATETIME NOT NULL,
+-- TODO: Add Audit tables
+
+CREATE TABLE tPerson (
+id INTEGER NOT NULL PRIMARY KEY ASC AUTOINCREMENT,
+name VARCHAR(255) NOT NULL
+);
+
+
+-- TODO: Create a tPlace for returns?
+-- TODO: CHECK constraint for coords?
+-- position_range - 1e-4 around 10m 
+CREATE TABLE tEvent (
+id INTEGER NOT NULL PRIMARY KEY ASC AUTOINCREMENT,
+name VARCHAR(255) NOT NULL,
+time_stamp TIMESTAMP NOT NULL,
 latitude DECIMAL(9,6) NOT NULL,
 longitude DECIMAL(9,6) NOT NULL,
-position_range DECIMAL(9,6) DEFAULT 0.0001, --1e-4 around 10m 
-extra_details TEXT,
-PRIMARY KEY (id)
-)
+position_range DECIMAL(9,6) DEFAULT 0.0001,
+extra_details TEXT DEFAULT NULL
+);
+
+-- TODO: Create indexes for searching
 
 
-
-
-CREATE TABLE tTransaction
-(
-id int NOT NULL,
+-- Remember, amounts from the perspective of the pot!
+CREATE TABLE tTransaction (
+id INTEGER NOT NULL PRIMARY KEY ASC AUTOINCREMENT,
 amount DECIMAL(5,2),
-PRIMARY KEY (id)
-)
+person NOT NULL REFERENCES tPerson(id),
+event NOT NULL REFERENCES tEvent(id)
+);
